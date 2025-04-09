100fd46d9b15c64f92c3cc1e96e76e6eb8853b6c
ZuidVolt <czuid@icloud.com>
2025-04-09 16:34:18 +0800

    Add functions to read word lists from files for q9

--- main.py
+++ main.py
@@ -23,6 +23,30 @@ def score_guess(user_guess, target_word):
     return score_list
 
 
+# Cooper, 20146487, 9/4/25
+def read_file(file_path):
+    with open(file_path, encoding="utf-8") as file_handler:
+        data = file_handler.read()
+        word_list = []
+        for line in data.splitlines():
+            word_list.append(line.strip())
+        return word_list
+
+
+def get_valid_words():
+    file_path = "./data/all_words.txt"
+    word_list = read_file(file_path)
+    print("valid_words:", word_list)  # debug
+    return word_list
+
+
+def get_target_words():
+    file_path = "./data/target_words.txt"
+    word_list = read_file(file_path)
+    print("target words:", word_list)  # debug
+    return word_list
+
+
 def tests():
     test_case_1 = score_guess("world", "world")
     test_case_2 = score_guess("world", "hello")
@@ -33,7 +57,9 @@ def tests():
 
 
 def main():
-    tests()
+    # tests()
+    # get_valid_words()
+    get_target_words()
 
 
 if __name__ == "__main__":
