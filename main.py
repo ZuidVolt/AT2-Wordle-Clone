18fbad35650e67978008a6c70158c32574b3d083
ZuidVolt <czuid@icloud.com>
2025-04-09 15:37:46 +0800

    Add scoring function for word guesses
    Implement score_guess function to compare user guesses to target words
    and return a list of scores. Add test cases to verify functionality.
    this is progress up too q4 of part b

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 """Program entry point"""
 
 
+# Cooper, 20146487, 9/4/25
+def score_guess(user_guess, target_word):
+    score_list: list[int] = [0] * len(target_word)
+    if user_guess == target_word:
+        for i in range(len(score_list)):
+            score_list[i] = 2
+    return score_list
+
+
+def tests():
+    test_case_1 = score_guess("world", "world")
+    test_case_2 = score_guess("world", "hello")
+    assert test_case_1 == [2, 2, 2, 2, 2], "should be [2, 2, 2, 2, 2]"
+    print(test_case_1)  # returns [2, 2, 2, 2, 2]
+    assert test_case_2 == [0, 0, 0, 0, 0], "should be [0, 0, 0, 0, 0]"
+    print(test_case_2)  # returns [0, 0, 0, 0, 0]
+
+
 def main():
-    """Main function"""
+    tests()
 
 
 if __name__ == "__main__":
