2ed52c9789ad71060207b3e9a9b6e31b1163d734
ZuidVolt <czuid@icloud.com>
2025-04-28 16:26:35 +0800

    started logic on game loop with user_input, score calc and info display to the cli

--- wordle.py
+++ wordle.py
@@ -75,7 +75,7 @@ def print_display_list(display_list):
     )
 
 
-def display_guess_result(scored_guess):
+def get_display_list(scored_guess) -> list[str]:
     display_list = []
     for i in scored_guess:
         if i == 0:
@@ -89,15 +89,30 @@ def display_guess_result(scored_guess):
                 "A scored_guess Should only have elements values from 0-2 got:",
                 i,
             )
-    print_display_list(display_list)
+    return display_list
 
 
-def game_display():
-    pass
+def display_past_guesses(past_valid_guesses_list):
+    for past_valid_words, past_display_lists in past_valid_guesses_list:
+        print_display_list(past_valid_words)
+        print_display_list(past_display_lists)
 
 
-def user_input():
-    pass
+def prompt_and_return_guess():
+    user_guess = input("Enter your guess: ").lower()
+    return user_guess
+
+
+def is_valid_user_guess(user_guess, valid_words_list):
+    return (
+        user_guess in valid_words_list
+    )  # don't need to worry about len case as all valid words are the same length
+
+
+def user_input(valid_words_list):
+    user_guess = prompt_and_return_guess()
+    is_valid = is_valid_user_guess(user_guess, valid_words_list)
+    return user_guess, is_valid
 
 
 def game_setup(
@@ -106,7 +121,7 @@ def game_setup(
     number_of_user_guesses=None,
 ):
     if valid_words_list == None:
-        valid_words_list = get_target_words()
+        valid_words_list = get_valid_words()
     if target_words_list == None:
         target_words_list = get_target_words()
     if number_of_user_guesses == None:
@@ -114,12 +129,57 @@ def game_setup(
     return valid_words_list, target_words_list, number_of_user_guesses
 
 
-def game_loop(game_setup_param=None):
+import random
+
+
+def get_random_word_target_word(valid_words_list):
+    return random.choice(valid_words_list)
+
+
+def game_loop(
+    game_setup_param=None,
+    mock_user_vaild_guesses: list[str] | None = None,
+):
     if game_setup_param == None:
         setup_data = game_setup()
     else:
         setup_data = game_setup_param
-    _valid_words_list, _target_words_list, _number_of_user_guesses = setup_data
+    valid_words_list, target_words_list, number_of_user_guesses = setup_data
+
+    past_valid_guesses_list: list[tuple[str, list[str]]] = []
+    user_won_the_game = False
+
+    target_word = get_random_word_target_word(target_words_list)
+    print(target_word)  # debug
+
+    print("Welcome to Wordle!")
+    print(f"You have {number_of_user_guesses} guesses.")
+
+    for turn_number in range(number_of_user_guesses):
+        display_past_guesses(past_valid_guesses_list)
+        while True:
+            if not mock_user_vaild_guesses == None:
+                user_guess = mock_user_vaild_guesses[turn_number - 1]
+                break
+            else:
+                print("-" * 20)
+                user_guess, is_valid = user_input(valid_words_list)
+                # todo add Logic to check if word has already been guessed
+                if is_valid:
+                    break
+            print("Invalid guess. Please try again.")
+        guess_result = score_guess(user_guess, target_word)
+        if guess_result == (2, 2, 2, 2, 2):
+            user_won_the_game = True
+            break
+        else:
+            display_guess = get_display_list(guess_result)
+            past_valid_guesses_list.append((user_guess, display_guess))
+
+    if user_won_the_game:
+        print("Congratulations! You won!")
+    else:
+        print("Sorry, you lost.")
 
 
 def run_tests_quickly():
@@ -129,9 +189,18 @@ def run_tests_quickly():
 
 
 def main():
-    run_tests_quickly()
-    # x = (1, 2, 0, 0, 1)
+    game_loop()
+    # run_tests_quickly()
+
+    # # x = (1, 2, 0, 0, 1)
     # display_guess_result(x)
+
+    # past_valid_guesses_list: list[tuple[str, list[str]]] = [
+    #     ("hello", ["0", "X", "X", "X", "0"]),
+    #     ("world", ["0", "X", "X", "X", "0"]),
+    #     ("crane", ["0", "0", "X", "X", "0"]),
+    # ]
+    # display_past_guesses(past_valid_guesses_list)
 
 
 if __name__ == "__main__":
